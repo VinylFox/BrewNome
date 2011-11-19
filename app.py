9952c21ee14409407edcd9f8fb1f928a5c9272b7
Shea Frederick <justarock@gmail.com>
2011-11-19 12:56:39 -0500

    fixed tabbing

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 import web
 
-  urls = (
-      '/.*', 'hello',
-      )
+urls = ('/.*', 'hello')
 
-  class hello:
-      def GET(self):
-          return "Hello, world."
+class hello:
+  def GET(self):
+    return "Hello, world."
 
-  application = web.application(urls, globals()).wsgifunc()
+application = web.application(urls, globals()).wsgifunc()
